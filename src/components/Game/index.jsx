423df8368ba506d856c665686159e23d9cb052f3
Alyson Consuli <consulialyson@gmail.com>
2022-05-20 21:20:51 -0300

    feat: botao de restart e voltar pra home

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable react/jsx-pascal-case */
 import axios from "axios";
 import { useContext, useState } from "react"
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContext";
 import { Question } from "../Question"
 import { $Game } from "./style"
@@ -13,7 +14,8 @@ export const Game = () => {
     const promise = axios.get(URL)
     promise.then((res) => console.log(res.data))*/
 
-    const { user } = useContext(UserContext)
+    const { user, setUser } = useContext(UserContext)
+    const [reload, setReload] = useState(false)
 
     const [questions, setQuestions] = useState([
         {
@@ -41,10 +43,33 @@ export const Game = () => {
                     isCorrectAnswer: true
                 }
             ]
+        },
+        {
+            title: 'Titulo pergunta 3',
+            answers: [
+                {
+                    text: 'texto da resposta 1',
+                    isCorrectAnswer: false
+                },
+                {
+                    text: 'texto da resposta 2',
+                    isCorrectAnswer: true
+                }
+            ]
         }
     ])
 
     const total = Math.round(((user.correct) / (user.done) * 100).toFixed(2))
+    const navigate = useNavigate()
+
+    function restart() {
+        setUser({ ...user, done: 0, correct: 0 })
+        setReload(!reload)
+    }
+    function backToMain() {
+        setUser({ ...user, done: 0, correct: 0 })
+        navigate('/')
+    }
 
     return (
         <$Game>
@@ -60,7 +85,13 @@ export const Game = () => {
                 })}
             </article>
             {questions.length === user.done &&
-                <span>Você acertou {total}% das respostas!</span>
+                <>
+                    <span>Você acertou {total}% das respostas!</span>
+                    <div>
+                        <button onClick={restart} >Refazer Teste</button>
+                        <button onClick={backToMain} >Escolher outro Teste</button>
+                    </div>
+                </>
             }
         </$Game>
     )
